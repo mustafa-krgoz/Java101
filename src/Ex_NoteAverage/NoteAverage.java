0d07faa53eb6ac4b03fec82ec53fc45cf273cbb3
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2023-05-31 16:47:41 +0300

    Java101Path

---
+++ src/Ex_NoteAverage/NoteAverage.java
@@ -0,0 +1,28 @@
+package BasicProjects;
+import java.util.Scanner;
+
+public class NoteAverage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int maths, physics, chemistry, turkish, history, music;
+
+        System.out.print("Enter maths note:");
+        maths = input.nextInt();
+        System.out.print("Enter physics note:");
+        physics = input.nextInt();
+        System.out.print("Enter chemistry note:");
+        chemistry = input.nextInt();
+        System.out.print("Enter turkish note:");
+        turkish = input.nextInt();
+        System.out.print("Enter history note:");
+        history = input.nextInt();
+        System.out.print("Enter music note:");
+        music = input.nextInt();
+
+        double average = (maths + physics + chemistry + turkish + history + music) / 6.0;
+        String result = (average>60) ? "You pass the class" : "You failed the class";
+        System.out.println("Average:" + average +"\t"+ result);     
+
+    }
+}
