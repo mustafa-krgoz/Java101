fc0be8add84218abb804e14461a882d3a3eb3f41
Halit Mustafa Karagöz <93734376+mustafa-krgoz@users.noreply.github.com>
2023-05-31 16:55:57 +0300

    Java101Path

---
+++ src/BasicProjects/QuadrilateralCalculation.java
@@ -0,0 +1,15 @@
+package Input;
+
+public class QuadrilateralCalculation {
+    public static void main(String[] args) {
+        int shortSide = 7;
+        int longSide = 10;
+        double area, perimeter;
+        area = shortSide * longSide;
+        perimeter = 2*(shortSide + longSide);
+        System.out.println("Area:" + area + "\n" + "Perimeter:" + perimeter);
+
+    }
+
+
+}
